1cfdac82a462ba84ca473e50745da6bd0ccb485e
Emmanuel Ojeah <ojeahemmanuel@outlook.com>
2020-07-17 20:29:03 +0100

    configure redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,32 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
 import * as courseActions from '../../redux/actions/courseActions';
 import { bindActionCreators } from 'redux';
 
 const CoursesPage = props => {
-    const [course, setCourse] = useState({ title: '' });
-
-    const handleChange = event => {
-        const course = { ...course, title: event.target.value };
-        setCourse(course);
-    };
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        props.actions.createCourse(course); //(courseActions.createCourse(course));
-    };
+    useEffect(() => {
+        props.actions.loadCourses().catch(error => {
+            alert('Loading courses failed ' + error);
+        });
+    }, []);
 
     return (
-        <form onSubmit={handleSubmit}>
+        <>
             <h2>Courses</h2>
-            <h3>Add Course</h3>
-            <input type='text' onChange={handleChange} value={course.title} />
-            <input type='submit' value='Save' />
             {props.courses.map(c => (
                 <div key={c.title}>{c.title}</div>
             ))}
-        </form>
+        </>
     );
 };
 
